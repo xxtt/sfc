41719bb0f826c375fef26019735b5c702faf4725
xx <8848484@gmail.com>
2014-11-10 02:44:56 +0200

    almost done

--- js/language.js
+++ js/language.js
@@ -1,12 +1,20 @@
-LANGUAGE = "ua";
 ru = document.getElementById("ru");
 ua = document.getElementById("ua");
 us = document.getElementById("us");
-categoryText = document.getElementById("category");
-placeText = document.getElementById("place");
-contactsText = document.getElementById("contacts");
-p_country = document.getElementById("p_country");
-p_city = document.getElementById("p_city");
+
+portal = document.getElementById("portal");
+
+h1 = document.getElementById("h1");
+text1 = document.getElementById("text1");
+h2 = document.getElementById("h2");
+text2 = document.getElementById("text2");
+h3 = document.getElementById("h3");
+text3 = document.getElementById("text3");
+
+more1 = document.getElementById("more1");
+more2 = document.getElementById("more2");
+more3 = document.getElementById("more3");
+
 
 
 function changeRu() {
@@ -14,36 +22,37 @@ function changeRu() {
     ua.style.color = "white";
     us.style.color = "white";
 
-    categoryText.innerHTML = "выбор рубрики";
-    placeText.innerHTML = "список мест";
-    contactsText.innerHTML = "КОНТАКТЫ";
-    p_country.innerHTML = "страна";
-    p_city.innerHTML = "город";
+    portal.innerHTML = "информационный медиа портал";
 
-    LANGUAGE = "ru";
+    h1.innerHTML = "Медиа Именинник";
+    text1.innerHTML = "Text 1"
+    h2.innerHTML = "Медиа Новость";
+    text2.innerHTML = "Text 2"
+    h3.innerHTML = "Рекомендуем Фильм";
+    text3.innerHTML = "Text 3"
 
-    initCategoryList();
-    showCategory();
-    initContactMenus();
-
+    more1.innerHTML = more2.innerHTML = more3.innerHTML = "читать дальше ..."
 }
 
 function changeUa() {
     ru.style.color = "white";
     ua.style.color = "red";
     us.style.color = "white";
+    portal.innerHTML = "інформаційний медіа портал";
 
-    categoryText.innerHTML = "вибір рубрики";
-    placeText.innerHTML = "список місць";
-    contactsText.innerHTML = "КОНТАКТИ";
-    p_country.innerHTML = "країна";
-    p_city.innerHTML = "місто";
+    h1.innerHTML = "Медіа Іменинник";
+    text1.innerHTML = "Text 1"
 
-    LANGUAGE = "ua";
+    h2.innerHTML = "Медіа Новина";
+    text2.innerHTML = "Text 2"
 
-    initCategoryList();
-    showCategory();
-    initContactMenus();
+    h3.innerHTML = "Рекомендуємо Фільм";
+    text3.innerHTML = "Text 3"
+
+    more1.innerHTML = more2.innerHTML = more3.innerHTML = "читати далі ..."
+
+    text1.innerHTML = text2.innerHTML = text3.innerHTML = "Lorem ipsum dolor sit amet, in sanctus delenit eleifend vim, mel consul inimicus voluptatum ex. Nisl duis posse in per, sea ut vidit choro copiosae, has ex case omittam expetendis. Eu dico tacimates mandamus mei, delicata adversarium quo in, te prima persecuti eum. At tale eros est, sea ad alii nostrum recusabo. Ea quaeque perfecto delicata est. Tation offendit nam cu. Ne harum admodum qui, nam id aperiri efficiantur delicatissimi. Autem graeco ex pro. Ei vidit nulla sit. Ad illud oportere sit, discere saperet per cu. Pro prima tempor impetus cu. Ut duo duis aperiri copiosae, ex vide doctus nostrud pri. Prima omnesque invenire sit cu, duo legere accusamus efficiantur at, ea mel vidisse iracundia. Populo inermis sit et. Ex commodo alterum vim. Sit possim tamquam perpetua ne, ea munere sanctus vis."
+
 }
 
 function changeUs() {
@@ -51,17 +60,19 @@ function changeUs() {
     ua.style.color = "white";
     us.style.color = "red";
 
-    categoryText.innerHTML = "choose place";
-    placeText.innerHTML = "place list";
-    contactsText.innerHTML = "CONTACTS";
-    p_country.innerHTML = "country";
-    p_city.innerHTML = "city";
+    portal.innerHTML = "information media portal";
 
-    LANGUAGE = "us";
+    h1.innerHTML = "Media Birthday";
+    text1.innerHTML = "Text 1"
 
-    initCategoryList();
-    showCategory();
-    initContactMenus();
+    h2.innerHTML = "Media News";
+    text2.innerHTML = "Text 2"
+
+    h3.innerHTML = "Recommended Movie";
+    text3.innerHTML = "Text 3"
+
+   more1.innerHTML = more2.innerHTML = more3.innerHTML = "read more ..."
+
 }
 
 changeUa();
