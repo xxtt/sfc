e65f361b9652aa99337166fb8a602a8ca6a52f53
xx <8848484@gmail.com>
2014-11-10 17:21:34 +0200

    modal window

--- js/language.js
+++ js/language.js
@@ -6,6 +6,9 @@ portal = document.getElementById("portal");
 
 h1 = document.getElementById("h1");
 text1 = document.getElementById("text1");
+mh1 = document.getElementById("mh1");
+mtext1 = document.getElementById("mtext1");
+
 h2 = document.getElementById("h2");
 text2 = document.getElementById("text2");
 h3 = document.getElementById("h3");
@@ -15,7 +18,7 @@ more1 = document.getElementById("more1");
 more2 = document.getElementById("more2");
 more3 = document.getElementById("more3");
 
-
+text = "Lorem ipsum dolor sit amet, in sanctus delenit eleifend vim, mel consul inimicus voluptatum ex. Nisl duis posse in per, sea ut vidit choro copiosae, has ex case omittam expetendis. Eu dico tacimates mandamus mei, delicata adversarium quo in, te prima persecuti eum. At tale eros est, sea ad alii nostrum recusabo. Ea quaeque perfecto delicata est. Tation offendit nam cu. Ne harum admodum qui, nam id aperiri efficiantur delicatissimi. Autem graeco ex pro. Ei vidit nulla sit. Ad illud oportere sit, discere saperet per cu. Pro prima tempor impetus cu. Ut duo duis aperiri copiosae, ex vide doctus nostrud pri. Prima omnesque invenire sit cu, duo legere accusamus efficiantur at, ea mel vidisse iracundia. Populo inermis sit et. Ex commodo alterum vim. Sit possim tamquam perpetua ne, ea munere sanctus vis.";
 
 function changeRu() {
     ru.style.color = "red";
@@ -24,14 +27,16 @@ function changeRu() {
 
     portal.innerHTML = "информационный медиа портал";
 
-    h1.innerHTML = "Медиа Именинник";
-    text1.innerHTML = "Text 1"
-    h2.innerHTML = "Медиа Новость";
-    text2.innerHTML = "Text 2"
-    h3.innerHTML = "Рекомендуем Фильм";
-    text3.innerHTML = "Text 3"
+    mh1.innerHTML = h1.innerHTML = "Медиа Именинник";
+    mtext1.innerHTML = text1.innerHTML = text;
 
-    more1.innerHTML = more2.innerHTML = more3.innerHTML = "читать дальше ..."
+    mh2.innerHTML = h2.innerHTML = "Медиа Новость";
+    mtext2.innerHTML = text2.innerHTML = text;
+
+    mh3.innerHTML = h3.innerHTML = "Рекомендуем Фильм";
+    mtext3.innerHTML = text3.innerHTML = text;
+
+    more1.innerHTML = more2.innerHTML = more3.innerHTML = "читать дальше ...";
 }
 
 function changeUa() {
@@ -40,18 +45,18 @@ function changeUa() {
     us.style.color = "white";
     portal.innerHTML = "інформаційний медіа портал";
 
-    h1.innerHTML = "Медіа Іменинник";
-    text1.innerHTML = "Text 1"
+    mh1.innerHTML = h1.innerHTML = "Медіа Іменинник";
+    mtext1.innerHTML = text1.innerHTML = text;
 
-    h2.innerHTML = "Медіа Новина";
-    text2.innerHTML = "Text 2"
+    mh2.innerHTML = h2.innerHTML = "Медіа Новина";
+    mtext2.innerHTML = text2.innerHTML = text;
 
-    h3.innerHTML = "Рекомендуємо Фільм";
-    text3.innerHTML = "Text 3"
 
-    more1.innerHTML = more2.innerHTML = more3.innerHTML = "читати далі ..."
+    mh3.innerHTML = h3.innerHTML = "Рекомендуємо Фільм";
+    mtext3.innerHTML = text3.innerHTML = text;
 
-    text1.innerHTML = text2.innerHTML = text3.innerHTML = "Lorem ipsum dolor sit amet, in sanctus delenit eleifend vim, mel consul inimicus voluptatum ex. Nisl duis posse in per, sea ut vidit choro copiosae, has ex case omittam expetendis. Eu dico tacimates mandamus mei, delicata adversarium quo in, te prima persecuti eum. At tale eros est, sea ad alii nostrum recusabo. Ea quaeque perfecto delicata est. Tation offendit nam cu. Ne harum admodum qui, nam id aperiri efficiantur delicatissimi. Autem graeco ex pro. Ei vidit nulla sit. Ad illud oportere sit, discere saperet per cu. Pro prima tempor impetus cu. Ut duo duis aperiri copiosae, ex vide doctus nostrud pri. Prima omnesque invenire sit cu, duo legere accusamus efficiantur at, ea mel vidisse iracundia. Populo inermis sit et. Ex commodo alterum vim. Sit possim tamquam perpetua ne, ea munere sanctus vis."
+    more1.innerHTML = more2.innerHTML = more3.innerHTML = "читати далі ...";
+
 
 }
 
@@ -62,16 +67,16 @@ function changeUs() {
 
     portal.innerHTML = "information media portal";
 
-    h1.innerHTML = "Media Birthday";
-    text1.innerHTML = "Text 1"
+    mh1.innerHTML = h1.innerHTML = "Media Birthday";
+    mtext1.innerHTML = text1.innerHTML = text;
 
-    h2.innerHTML = "Media News";
-    text2.innerHTML = "Text 2"
+    mh2.innerHTML = h2.innerHTML = "Media News";
+    mtext2.innerHTML = text2.innerHTML = text;
 
-    h3.innerHTML = "Recommended Movie";
-    text3.innerHTML = "Text 3"
+    mh3.innerHTML = h3.innerHTML = "Recommended Movie";
+    mtext3.innerHTML = text3.innerHTML = text;
 
-   more1.innerHTML = more2.innerHTML = more3.innerHTML = "read more ..."
+    more1.innerHTML = more2.innerHTML = more3.innerHTML = "read more ...";
 
 }
 
